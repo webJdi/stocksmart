c5475ecb6a9b7d05edd28a3b5f2879f93a83a8d7
Suman Senapati <sigma.6.2009@gmail.com>
2024-08-19 02:07:48 +0530

    Updated firebase.js

--- voila/app/firebase.js
+++ voila/app/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 //import {firebaseConfig} from "./firebaseConfig.js"
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,7 +17,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app)
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
 
 export {app, firestore}
